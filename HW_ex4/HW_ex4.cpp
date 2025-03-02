bc5ccdf4f2beadb1ac42e7e227ba5cce76de78c1
DaniiLord <daniilsveckarev07@gmail.com>
2025-03-02 19:42:08 +0200

    wse

---
+++ HW_ex4/HW_ex4.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char C = '.';
+    string S = "hello world. hello step. hello it.";
+    string S0 = " good";
+
+    for (size_t i = 0; i < S.length(); ++i) {
+        if (S[i] == C) {
+            S.insert(i, S0);
+            i += S0.length();
+        }
+    }
+
+    cout << S << endl;
+
+    return 0;
+}
